9b2ee56fa1656845011d6022bab4aa3515ea2f74
Tiia <tiiatrogen@gmail.com>
2019-01-14 21:00:16 +0200

    Fetching gives a 404

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,17 +1,15 @@
-// import React, { Component } from 'react';
+import React from 'react';
 
-// const Card = (props) => {
-   
-//     return (
-//         <div style={{margin: '1em'}}>
-//             <img width="75" alt="" src={props.avatar_url} />
-//             <div style={{display: 'inline-block', marginLeft: 10, fontFamily: 'sans-serif', position: "absolute"}}>
-//                 <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
-//                 <div>{props.company}</div>
-//             </div>
-//         </div>
-//     );
-// };
-
-
-// export default Card;
+const Card = (props) => {
+    return (
+        <div style={{margin: '1em'}}>
+            <img width="75" alt="" src={props.avatar_url} />
+            <div style={{display: 'inline-block', marginLeft: 10, fontFamily: 'sans-serif', position: "absolute"}}>
+                <div style={{fontSize: '1.25em', fontWeight: 'bold'}}>{props.name}</div>
+                <div>{props.company}</div>
+            </div>
+        </div>
+    );
+  };
+  
+export default Card;
